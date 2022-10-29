e7357c6947faab9f62f92269162f8236dffcd943
Fernando Martinez <fernando.martinez.swe@gmail.com>
2022-10-29 14:48:52 -0700

    Even or Odd task

---
+++ 8kyu/even-or-odd.js
@@ -0,0 +1,18 @@
+//Description:
+
+//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+//My solution
+function evenOrOdd(number) {
+  if(number % 2 === 0){
+    return 'Even'
+  }
+  else{
+    return 'Odd'
+  }
+}
+
+//Other solutions
+function even_or_odd(number) {
+  return number % 2 ? "Odd" : "Even"
+}
