85215e250c268fbf85b9c8ba8257e7400b499d37
fmartinez-swe <116055295+fmartinez-swe@users.noreply.github.com>
2022-12-16 22:56:31 -0800

    My solution to a 8kyu challenge - 'Pythagorean Triple'

---
+++ 8kyu/pythagorean-triple.js
@@ -0,0 +1,46 @@
+//Problem
+/*
+Description:
+
+Given an unsorted array of 3 positive integers [ n1, n2, n3 ], determine if it is possible to form a Pythagorean Triple using those 3 integers.
+
+A Pythagorean Triple consists of arranging 3 integers, such that:
+
+a2 + b2 = c2
+Examples
+
+[5, 3, 4] : it is possible to form a Pythagorean Triple using these 3 integers: 32 + 42 = 52
+
+[3, 4, 5] : it is possible to form a Pythagorean Triple using these 3 integers: 32 + 42 = 52
+
+[13, 12, 5] : it is possible to form a Pythagorean Triple using these 3 integers: 52 + 122 = 132
+
+[100, 3, 999] : it is NOT possible to form a Pythagorean Triple using these 3 integers - no matter how you arrange them, you will never find a way to satisfy the equation a2 + b2 = c2
+Return Values
+
+    For Python: return True or False
+    For JavaScript: return true or false
+    Other languages: return 1 or 0 or refer to Sample Tests.
+
+*/
+
+//My solution
+function isPythagoreanTriple(integers) {
+  integers.sort((a,b) => (a - b))
+  let aSquared = integers[0]**2
+  let bSquared = integers[1]**2
+  let cSquared = integers[2]**2
+
+  if(aSquared + bSquared === cSquared){
+    return true
+  }else{
+    return false
+  }
+
+}
+
+//Other solutions
+function isPythagoreanTriple(integers) {
+  let massiv = integers.sort((a, b) => a - b)
+  return (massiv[0] ** 2 + massiv[1] ** 2 == massiv[2] ** 2)
+}
