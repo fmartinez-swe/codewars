3b59f3ce4bcc0efb32645fe2bd7b7b86f62e43ef
fmartinez-swe <116055295+fmartinez-swe@users.noreply.github.com>
2022-12-19 20:14:37 -0800

    My solution to a 7kyu codewars - 'cats and shelves'

---
+++ 7kyu/cats-and-shelves.js
@@ -0,0 +1,66 @@
+//Problem
+
+/*
+Description:
+Description
+
+An infinite number of shelves are arranged one above the other in a staggered fashion.
+The cat can jump up to 3 shelves at the same time: from shelf 1 to shelf 2 or 4 (the cat cannot climb on the shelf directly above its head), according to the illustration:
+
+                 ┌────────┐
+                 │-6------│
+                 └────────┘
+┌────────┐
+│------5-│
+└────────┘  ┌─────► OK!
+            │    ┌────────┐
+            │    │-4------│
+            │    └────────┘
+┌────────┐  │
+│------3-│  │
+BANG!────┘  ├─────► OK!
+  ▲  |\_/|  │    ┌────────┐
+  │ ("^-^)  │    │-2------│
+  │ )   (   │    └────────┘
+┌─┴─┴───┴┬──┘
+│------1-│
+└────────┘
+
+Input
+
+Start and finish shelf numbers (always positive integers, finish no smaller than start)
+Task
+
+Find the minimum number of jumps to go from start to finish
+Example
+
+Start 1, finish 5, then answer is 2 (1 => 4 => 5 or 1 => 2 => 5)
+*/
+
+//My solution
+function solution(start, finish)
+{
+  let steps = 0
+  while(start < finish){
+    if((start + 3) <= finish ){
+      start = start + 3
+      steps++
+    }else{
+      start = start + 1
+      steps++
+    }
+
+  }
+  return steps;
+}
+
+//Other solutions
+const solution = (start, finish, difference = finish - start) =>
+  Math.floor(difference / 3) + difference % 3
+
+//
+function solution(start, finish)
+{
+  let remainder = (finish - start) % 3;
+  return ((finish - start) - remainder) / 3 + remainder;
+}
