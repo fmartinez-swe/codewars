5ff8938a4baad28261f76301c317ee14ca3cd704
fmartinez-swe <116055295+fmartinez-swe@users.noreply.github.com>
2022-12-23 18:50:52 -0800

    My solution to an 8kyu - 'square(n) sum'

---
+++ 8kyu/square-sum.js
@@ -0,0 +1,17 @@
+/*
+Problem
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+*/
+
+//My solution
+
+function squareSum(numbers){
+    let arr = []
+    for(let i = 0; i < numbers.length; i++){
+      arr.push(numbers[i]**2)
+    }
+    let answer = arr.reduce((a, b) => a + b, 0)
+    return answer
+  }
